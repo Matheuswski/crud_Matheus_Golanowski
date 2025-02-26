e2855cfce8f4f91c1372fada204b8be5911a2e8c
MatheusG <mariaoglanowski@gmail.com>
2025-02-25 21:03:36 -0300

    Criação de Banco de Dados

---
+++ MatheusGolanowski_db.sql
@@ -0,0 +1,9 @@
+create database MatheusGolanowski_db
+
+	indusuario int not null auto_increment,
+    nome text,
+    telefone text,
+    usuario text,
+    senha text,
+    primary key(idusuario)
+);    
